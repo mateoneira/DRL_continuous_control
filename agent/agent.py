8eca4bc44edbdff6296b9ab1a56e71c46e2e5176
Mateo Neira <mateo0719@gmail.com>
2020-08-03 22:24:47 +0100

    updated agent hyperparameters and model architecture

--- agent/agent.py
+++ agent/agent.py
@@ -23,9 +23,9 @@ BUFFER_SIZE = int(1e6)  # replay buffer size
 BATCH_SIZE = 128        # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 10e-4        # learning rate of the actor 
-LR_CRITIC = 10e-3       # learning rate of the critic
-WEIGHT_DECAY = 10e-2    # L2 weight decay
+LR_ACTOR = 1e-4        # learning rate of the actor 
+LR_CRITIC = 1e-3       # learning rate of the critic
+WEIGHT_DECAY = 1e-2    # L2 weight decay
 
 NOISE_THETA = 0.15		# Ornstein-Ulenbeck parameter
 NOISE_SIGMA = 0.2		# Ornstein-Ulenbeck parameter
--- agent/model.py
+++ agent/model.py
@@ -62,7 +62,7 @@ class Critic(nn.Module):
 	actions are note included until the second layer, as per original paper
 	"""
 
-	def __init__(self, state_size, action_size, seed, fc1_units=256, fc2_units=256, fc3_units=128):
+	def __init__(self, state_size, action_size, seed, fc_units=256):
 		"""
 		Initialize parameters and build model
 
@@ -82,24 +82,22 @@ class Critic(nn.Module):
 
 		super(Critic, self).__init__()
 		self.seed = torch.manual_seed(seed)
-		self.fc1 = nn.Linear(state_size, fc1_units)
-		self.fc2 = nn.Linear(fc1_units + action_size, fc2_units)
-		self.fc3 = nn.Linear(fc2_units, fc3_units)
-		self.fc4 = nn.Linear(fc3_units, 1)
+		self.fc1 = nn.Linear(state_size, fc_units)
+		self.fc2 = nn.Linear(fc_units + action_size, fc_units/2)
+		self.fc3 = nn.Linear(fc_units/2, 1)
 		self.reset_parameters()
 
 	def reset_parameters(self):
 		self.fc1.weight.data.uniform_(*hidden_unit(self.fc1))
 		self.fc2.weight.data.uniform_(*hidden_unit(self.fc2))
-		self.fc3.weight.data.uniform_(*hidden_unit(self.fc3))
-		self.fc4.weight.data.uniform_(-3e3,3e3)
+		self.fc3.weight.data.uniform_(-3e3,3e3)
 
 	def forward(self, state, action):
 		x_state = F.relu(self.fc1(state))
 		x = torch.cat((x_state, action), dim=1)
 		x = F.relu(self.fc2(x))
-		x = F.relu(self.fc3(x))
-		return self.fc4(x)
+		# x = F.relu(self.fc3(x))
+		return self.fc3(x)
 	
 
 
